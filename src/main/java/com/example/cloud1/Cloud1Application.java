53922e050400fb7c22301fd4e8bbb2b54349f450
Cédric Staelens <Cedric.Staelens@arhs-technology.com>
2023-01-24 15:46:48 +0100

    Change project

--- src/main/java/com/example/cloud1/Cloud1Application.java
+++ src/main/java/com/example/cloud1/Cloud1Application.java
@@ -2,9 +2,7 @@ package com.example.cloud1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class Cloud1Application {
 
